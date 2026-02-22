584991bc30e614180deb7bcd983d972402682437
Uzair-Waseem-390 <uzairwaseem390@gmail.com>
2026-02-22 19:15:02 +0500

    frontend is almost ready

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-light'>Footer</div>
+    <>
+    <footer className="footer py-3 my-3">
+      <hr className='border-bottom'/>
+      <p className='text-light text-center'>&copy; 2026 Stock Prediction App. All rights reserved | Build By <a className="text-info " href="https://www.linkedin.com/in/uzair-waseem-digital/">Uzair Waseem</a></p>
+    </footer>
+    </>
   )
 }
 
--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div className='text-light'>Main</div>
+    <>
+    <div className="container">
+      <div className="p-5 text-center bg-light-dark rounded-3">
+        <h1 className='text-light'>Welcome to Stock Prediction App</h1>
+        <p className='text-light lead'>Predict stock prices with our advanced AI models</p>
+        <a className='btn btn-outline-info' href="">Login</a>
+      </div>
+    </div>
+    </>
   )
 }
 
